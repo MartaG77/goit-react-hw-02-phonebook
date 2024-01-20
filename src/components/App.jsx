3631a17c61263bab33fe0193c8e0884ddfa9d5e8
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2024-01-20 20:16:32 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
 export const App = () => {
+  const [phonebook, setPhonebook] = useState({
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  });
+
+  const filteredContacts = phonebook.contacts?.filter(contact =>
+    contact.name.toLowerCase().includes(phonebook.filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm phonebook={phonebook} setPhonebook={setPhonebook} />
+
+      <h2>Contacts</h2>
+      <Filter phonebook={phonebook} setPhonebook={setPhonebook} />
+      <ContactList
+        phonebook={phonebook}
+        setPhonebook={setPhonebook}
+        filteredContacts={filteredContacts}
+      />
     </div>
   );
 };
